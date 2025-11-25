78f5df39e17b2524f515632f4d0e04b2f8b229c5
VivekSathwara <vivekbsuvan123@gmail.com>
2025-11-26 00:20:33 +0530

    eight day done!

--- Array/Move_zeroes.py
+++ Array/Move_zeroes.py
@@ -19,13 +19,33 @@
 # Input: nums = [0]
 # Output: [0]
 
-
-
-
 class Solution(object):
     def moveZeroes(self, nums):
-        
-        
+
+        nums.sort(reverse=True);
+        j = 0 
+        n = len(nums)
+        print(nums)
+        while j < n - 1:
+            print(nums[j],nums[j+1])
+            if nums[j] == 0:    
+                break;
+            elif nums[j] > nums[j + 1]: 
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                j = 0
+            j += 1
+        return nums;
+
+def main():
+
+    nums = [0,1,0,3,12]
+
+    s=Solution();
+    ans = s.moveZeroes(nums);
+    print(ans)
+
+main();
+
 
 
 
